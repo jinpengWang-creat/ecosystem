d9957ef57340520031ad9e5f1c127be0f53e19c4
wjp <2435878928@qq.com>
2024-05-23 20:10:42 +0800

    feature: testcode for anyhow/thiserror

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
+use std::fs;
+
+use anyhow::Context;
+use ecosystem::MyError;
+
+fn main() -> Result<(), anyhow::Error> {
     println!("Hello, world!");
+    let filename = "README.md";
+    let _fd = fs::File::open(filename).with_context(|| {
+        println!("hhhhh");
+        "can not find file"
+    })?;
+    fail_with_error()?;
+    Ok(())
+}
+
+fn fail_with_error() -> Result<(), MyError> {
+    println!("{}", std::mem::size_of::<MyError>());
+    println!("{}", std::mem::size_of::<String>());
+    Err(MyError::Custom("This is a custom error".to_string()))
 }
