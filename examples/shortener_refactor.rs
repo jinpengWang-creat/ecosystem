8572f5f84f318d29863d1cb5f4963970be8a3aa0
wjp <2435878928@qq.com>
2024-06-04 16:35:47 +0800

    refactor: refactor shortener

---
+++ examples/shortener_refactor.rs
@@ -0,0 +1,11 @@
+use anyhow::Result;
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, Layer};
+#[tokio::main]
+async fn main() -> Result<()> {
+    // init tracing
+    let layer = fmt::layer().pretty().with_filter(LevelFilter::INFO);
+    tracing_subscriber::registry().with(layer).init();
+
+    Ok(())
+}
